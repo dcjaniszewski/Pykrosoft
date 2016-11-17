a44a5eb32d66ee421750169d254960c6c20de74b
dcjaniszewski <dcjaniszewski@mix.wvu.edu>
2016-11-17 12:09:00 -0500

    First instance of php working

--- createelection.php
+++ createelection.php
@@ -1,10 +1,30 @@
 <?php
-	$dom = new DOMDocument();
-	$dom.load("election.html");
-	$filename = $dom.getElementById("eleccomminput");
-	
-	$myFile = fopen("/Users/dominicjaniszewski/Desktop/CS/html/Pykrosoft/test2.txt 
-","r") or die ("Unable to create");
-	fwrite($myFile,"test");
-	fclose($myFile);
+//libxml_use_internal_errors(true);
+$filename = $_GET['input'];
+$electionFile = fopen($filename.=".txt","w+"); // change x+ to something that creates, reads and writes.
+fclose($electionFile);
 ?>
+
+<html>
+<head>
+    <title>Election Created</title>
+    <link href = "stylesheet.css" rel = "stylesheet">
+</head>
+<body>
+<form name = "form" action = "createelection.php" method = "GET">
+    <div style = "height:600px; width: 500px;">
+        <ul>
+            <li id = "home"><a href = "mainpage.html">Home</a></li>
+            <li id = "news"><a href = "mainpage.html">News</a></li>
+            <li id = "prt"><a href = "mainpage.html">PRT Status</a></li>
+            <li id = "signout"><a href = "login.html">Sign out</a></li>
+        </ul>
+        <h1>Election Created</h1>
+    </div>
+</form>
+</body>
+
+
+
+</html>
+
